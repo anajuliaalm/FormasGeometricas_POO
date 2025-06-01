0d81c914b9ddb317f58f1a05816d4cf7af45c4ca
Ana Júlia Almeida <anajulia@aluno.ifce.edu.br>
2025-06-01 11:25:55 -0300

    Add files via upload

---
+++ FormasGeometricas-Herança/FormasGeometricasEspaciais/Esfera.java
@@ -0,0 +1,26 @@
+package br.edu.FormasGeometricasEspaciais;
+
+public class Esfera extends figuraEspacial{
+    private double raio;
+
+    public Esfera() {
+    	raio = 10;
+    	
+    } public void setRaio(double pRaio) {
+        raio = raio;
+    } public double getRaio() {
+        return raio;
+    } public void calcArea() {
+    	area = 4 * Math.PI * (raio * raio);
+    } public void calcVolume() {
+        volume = (4 / 3) * Math.PI * (raio  * raio * raio);
+    } public void calcAreaTotal() {
+		areaTotal = areaTotal;
+	} public void calcDiagonal() {
+		diagonal = diagonal;
+	} public void calcAreaTotalBase() {
+		 areaTotalBase = areaTotalBase;
+	} public void calcAlturaa() {
+		altura = altura;
+	}
+}
